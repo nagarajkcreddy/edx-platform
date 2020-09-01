57a4c3968b25a4f694a1c5159d03cbcd41fca4c6
uzairr <uzairr@yahoo.com>
2020-09-01 14:17:21 +0500

    Modify the api response
    Update the api response so that it cannot contain the response
    in the form of HTML which may prove vulnerable for MFE in future.
    
    VAN-14

--- openedx/core/djangoapps/user_authn/views/login.py
+++ openedx/core/djangoapps/user_authn/views/login.py
@@ -71,20 +71,17 @@ def _do_third_party_auth(request):
             u"with backend_name {backend_name}".format(
                 username=username, backend_name=backend_name)
         )
-        message = Text(_(
+        message = _(
             u"You've successfully signed in to your {provider_name} account, "
-            u"but this account isn't linked with your {platform_name} account yet. {blank_lines}"
+            u"but this account isn't linked with your {platform_name} account yet."
             u"Use your {platform_name} username and password to sign in to {platform_name} below, "
-            u"and then link your {platform_name} account with {provider_name} from your dashboard. {blank_lines}"
+            u"and then link your {platform_name} account with {provider_name} from your dashboard."
             u"If you don't have an account on {platform_name} yet, "
             u"click {register_label_strong} at the top of the page."
-        )).format(
-            blank_lines=HTML('<br/><br/>'),
+        ).format(
             platform_name=platform_name,
             provider_name=requested_provider.name,
-            register_label_strong=HTML('<strong>{register_text}</strong>').format(
-                register_text=_('Register')
-            )
+            register_label_strong='Register'
         )
 
         raise AuthFailedError(message)
